1cad622d6e84aa29e720eec037bee84e95f8ab72
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-07-12 19:07:30 -0500

    chat gpt en chatbot

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link'
-import styles from './Header.module.css'
 import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
+import styles from './Header.module.sass'
+import dynamic from 'next/dynamic'
+
+const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false })
 
 
 export const Header = async () => {
   const customer = await validateAccessToken()
 
   return (
-    <header>
+    <header className={styles.Header}>
       <nav>
         <ul className={styles.Header__list}>
           <li>
@@ -21,7 +24,10 @@ export const Header = async () => {
             </Link>
           </li>
         </ul>
-        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
       </nav>
+      <div className={styles.Header__user}>
+        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+        <NoSSRShoppingCart />
+      </div>
     </header>)
 }
